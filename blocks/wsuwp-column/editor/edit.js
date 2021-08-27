036363eb0ca93b17a0e591c4e507acaa040a9d0c
Dan White <dan.white1@wsu.edu>
2021-08-26 17:04:58 -0700

    Create ColorClassNameSelector as a reusable block-control
    Refactor the original POC into a reusable block-control. Also drastically simplifies the control by only supporting a single className for each color.

--- blocks/wsuwp-column/editor/edit.js
+++ blocks/wsuwp-column/editor/edit.js
@@ -1,139 +1,35 @@
-/*
-	Considerations:
-	1. Do classes exist currently? 
-		1.1 Utility classes vs component classes?
-	2. Handling text color and sub-components. Dark/light modes.
-	3. Grays to use.
-		0,5,10,75,85,95
-	4. Making into a block control. Make colors configurable.
-	5. Rendering in admin/front-end
-	6. Hanlding multiple colorSettings 
-*/
 
-const { __ } = wp.i18n;
 
-const { InnerBlocks } = wp.blockEditor;
-
-const {
-	InspectorControls,
-	BlockControls,
-	useBlockProps,
-	PanelColorSettings,
-	getColorObjectByColorValue
-} = wp.blockEditor;
-
-const {
-	PanelBody,
-	TextControl,
-	SelectControl,
-	Button,
-	FocalPointPicker,
-	BaseControl
-} = wp.components;
-
-const { 
-	find,
-	filter,
-	reduce,
-	uniq,
-	difference,
-	every,
-	includes
-} = lodash;
-
-const {
-	useState,
-	useEffect
-} = React;
+import { InnerBlocks, InspectorControls, useBlockProps} from '@wordpress/block-editor';
+import { ColorClassNameSelector } from '../../../assets/src/js/partials/block-controls/blockControls'
 
 const Edit = ( props ) => {
-	const {
-		className, 
-		isSelected, 
-		attributes, 
-		setAttributes,
-		colors = [			
-			{ name: 'Gray 0', color: '#f7f7f7', className: 't-light wsu-color-background--gray-0' },
-			{ name: 'Gray 5', color: '#f2f2f2', className: 'wsu-color-background--gray-5' },
-			{ name: 'Gray 10', color: '#e6e6e6', className: 'wsu-color-background--gray-10' },
-			{ name: 'Gray 75', color: '#404040', className: 't-dark wsu-color-background--gray-75' },
-			{ name: 'Gray 85', color: '#262626', className: 't-dark wsu-color-background--gray-85' },
-			{ name: 'Gray 95', color: '#080808', className: 't-dark wsu-color-background--gray-95' },
-		],
-		colors2 = [			
-			{ name: 'Gray 0', color: '#f7f7f7', className: 't-light wsu-color-text--gray-0' },
-			{ name: 'Gray 5', color: '#f2f2f2', className: 'wsu-color-text--gray-5' },
-			{ name: 'Gray 10', color: '#e6e6e6', className: 'wsu-color-text--gray-10' },
-		]
-	} = props;	
-
-	const [backgroundColor, setBackgroundColor] = useState();
-
 	const blockProps = useBlockProps( { 
         className: 'wsu-column', 
         style: {}, 
     } );
 
-	const colorClassNames = uniq(reduce(colors, (acc, cur) => {
-		return acc.concat(cur.className.split(' '));
-	}, []));	
-
-	function getColorObjectByClassName(colors, className){
-		if( !className ) { return; }
-
-		const appliedClasses = className.split(' ');
-
-		return find(colors, (color) => {
-			const colorClasses = color.className.split(' ');
-			return every(colorClasses, (c) => includes(appliedClasses, c));
-		});
-	}
-
-	const updateColor = (colorValue) => {
-		const colorObject = getColorObjectByColorValue(colors, colorValue);
-		let className = colorObject?.className ?? '';
-
-		if( attributes.className )
-		{
-			const appliedClasses = attributes.className.split(' ');
-			const newClasess = difference(appliedClasses, colorClassNames).concat(className);			
-			className = newClasess.map(function(c) {					
-					return c.trim();
-				})
-				.filter((c) => c)
-				.join(' ');			
-		}
-		
-		setAttributes( { className: className });
-	}
-
-	useEffect( () => {
-		setBackgroundColor(getColorObjectByClassName(colors, attributes.className)?.color);
-	}, [attributes.className])
-
     return (
 		<>
 			<InspectorControls>
-				<PanelColorSettings
-					title={ 'Color Settings'  }
-					colorSettings={ [
+				<ColorClassNameSelector
+					title="Color Settings"
+					colorSettings={[
 						{
-							colors: colors,
-							value: backgroundColor,
-							onChange: updateColor,
 							label: 'Background Color',
-						},
-						{
-							colors: colors2,
-							value: backgroundColor,
-							onChange: updateColor,
-							label: 'Background Color',
+							prefix: 'wsu-color-background--',
+							colors: [			
+								{ name: 'Gray 0', color: '#f7f7f7', className: 'gray-0' },
+								{ name: 'Gray 5', color: '#f2f2f2', className: 'gray-5' },
+								{ name: 'Gray 10', color: '#e6e6e6', className: 'gray-10' },
+								{ name: 'Gray 75', color: '#404040', className: 'gray-75' },
+								{ name: 'Gray 85', color: '#262626', className: 'gray-85' },
+								{ name: 'Gray 95', color: '#080808', className: 'gray-95' },
+							],
 						}
-					] }
-					disableCustomGradients={ true }
-					disableCustomColors={ true }
-				>
-				</PanelColorSettings>
+					]}
+					{...props}>
+				</ColorClassNameSelector>				
 			</InspectorControls>
 
 			<div { ...blockProps }  >
@@ -143,6 +39,208 @@ const Edit = ( props ) => {
 			</div>
 		</>
     )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 	const { __ } = wp.i18n;
+
+// const { InnerBlocks } = wp.blockEditor;
+
+// const {
+// 	InspectorControls,
+// 	BlockControls,
+// 	useBlockProps,
+// 	PanelColorSettings,
+// 	getColorObjectByColorValue
+// } = wp.blockEditor;
+
+// const {
+// 	PanelBody,
+// 	TextControl,
+// 	SelectControl,
+// 	Button,
+// 	FocalPointPicker,
+// 	BaseControl
+// } = wp.components;
+
+// const { 
+// 	find,
+// 	filter,
+// 	reduce,
+// 	uniq,
+// 	difference,
+// 	every,
+// 	includes
+// } = lodash;
+
+// const {
+// 	useState,
+// 	useEffect
+// } = React;
+
+// const Edit = ( props ) => {
+// 	const {
+// 		className, 
+// 		isSelected, 
+// 		attributes, 
+// 		setAttributes,
+// 		colors = [			
+// 			{ name: 'Gray 0', color: '#f7f7f7', className: 't-light wsu-color-background--gray-0' },
+// 			{ name: 'Gray 5', color: '#f2f2f2', className: 'wsu-color-background--gray-5' },
+// 			{ name: 'Gray 10', color: '#e6e6e6', className: 'wsu-color-background--gray-10' },
+// 			{ name: 'Gray 75', color: '#404040', className: 't-dark wsu-color-background--gray-75' },
+// 			{ name: 'Gray 85', color: '#262626', className: 't-dark wsu-color-background--gray-85' },
+// 			{ name: 'Gray 95', color: '#080808', className: 't-dark wsu-color-background--gray-95' },
+// 		],
+// 		colors2 = [			
+// 			{ name: 'Gray 0', color: '#f7f7f7', className: 't-light wsu-color-text--gray-0' },
+// 			{ name: 'Gray 5', color: '#f2f2f2', className: 'wsu-color-text--gray-5' },
+// 			{ name: 'Gray 10', color: '#e6e6e6', className: 'wsu-color-text--gray-10' },
+// 		]
+// 	} = props;	
+
+// 	const [backgroundColor, setBackgroundColor] = useState();
+
+// 	const blockProps = useBlockProps( { 
+//         className: 'wsu-column', 
+//         style: {}, 
+//     } );
+
+// 	const colorClassNames = uniq(reduce(colors, (acc, cur) => {
+// 		return acc.concat(cur.className.split(' '));
+// 	}, []));	
+
+// 	function getColorObjectByClassName(colors, className){
+// 		if( !className ) { return; }
+
+// 		const appliedClasses = className.split(' ');
+
+// 		return find(colors, (color) => {
+// 			const colorClasses = color.className.split(' ');
+// 			return every(colorClasses, (c) => includes(appliedClasses, c));
+// 		});
+// 	}
+
+// 	const updateColor = (colorValue) => {
+// 		const colorObject = getColorObjectByColorValue(colors, colorValue);
+// 		let className = colorObject?.className ?? '';
+
+// 		if( attributes.className )
+// 		{
+// 			const appliedClasses = attributes.className.split(' ');
+// 			const newClasess = difference(appliedClasses, colorClassNames).concat(className);			
+// 			className = newClasess.map(function(c) {					
+// 					return c.trim();
+// 				})
+// 				.filter((c) => c)
+// 				.join(' ');			
+// 		}
+		
+// 		setAttributes( { className: className });
+// 	}
+
+// 	useEffect( () => {
+// 		setBackgroundColor(getColorObjectByClassName(colors, attributes.className)?.color);
+// 	}, [attributes.className])
+
+//     return (
+// 		<>
+// 			<InspectorControls>
+// 				<PanelColorSettings
+// 					title={ 'Color Settings'  }
+// 					colorSettings={ [
+// 						{
+// 							colors: colors,
+// 							value: backgroundColor,
+// 							onChange: updateColor,
+// 							label: 'Background Color',
+// 						},
+// 						{
+// 							colors: colors2,
+// 							value: backgroundColor,
+// 							onChange: updateColor,
+// 							label: 'Background Color',
+// 						}
+// 					] }
+// 					disableCustomGradients={ true }
+// 					disableCustomColors={ true }
+// 				>
+// 				</PanelColorSettings>
+// 			</InspectorControls>
+
+// 			<div { ...blockProps }  >
+// 				<InnerBlocks
+// 					templateLock={ false }
+// 				/>
+// 			</div>
+// 		</>
+//     )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	/*{ 
 		if ( ! attributes.layout ) {
