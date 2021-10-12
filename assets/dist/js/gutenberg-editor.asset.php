7ec917c6700ec1d8e4fe091975d3103fad6c3fcf
Danial Bleile <danbleile@gmail.com>
2021-10-12 05:09:20 -0700

    Build | Files

--- assets/dist/js/gutenberg-editor.asset.php
+++ assets/dist/js/gutenberg-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => 'b03473220715d7bde00da61e99f90f68');
+<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => 'd502fc0a664cea82d09a28f0ae9022bc');
