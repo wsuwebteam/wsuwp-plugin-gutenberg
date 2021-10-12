cefa1a04994951e8290acd8c949bad151447af1c
Danial Bleile <danbleile@gmail.com>
2021-10-12 05:08:12 -0700

    Add Font Size Control

--- assets/src/js/partials/block-controls/blockControls.js
+++ assets/src/js/partials/block-controls/blockControls.js
@@ -4,3 +4,4 @@ export {default as FeedPostsControl } from './FeedPostsControl';
 export {default as PerRow } from './PerRowControl';
 export {default as ColorClassNameSelector } from './ColorClassNameSelector';
 export {default as SpacingClassNameSelector } from './spacing-class-name-selector/spacing-class-name-selector';
+export {default as FontSizeControl} from './FontSizeControl';
