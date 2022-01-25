21360846f6814e108fb344b9a9e2b48c2f00a5bf
Dan White <dan.white1@wsu.edu>
2022-01-25 10:38:46 -0800

    Version bump | 1.3.11

--- wsuwp-plugin-gutenberg.php
+++ wsuwp-plugin-gutenberg.php
@@ -3,7 +3,7 @@
  * Plugin Name: WSUWP Gutenberg
  * Plugin URI: https://github.com/wsuwebteam/wsuwp-plugin-gutenberg
  * Description: Describe the plugin
- * Version: 1.3.10
+ * Version: 1.3.11
  * Requires PHP: 7.3
  * Author: Washington State University, Danial Bleile
  * Author URI: https://web.wsu.edu/
@@ -16,7 +16,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define( 'WSUWPPLUGINGUTENBERGVERSION', '1.3.10' );
+define( 'WSUWPPLUGINGUTENBERGVERSION', '1.3.11' );
 
 add_action( 'after_setup_theme', 'wsuwp_plugin_gutenberg_init' );
 
