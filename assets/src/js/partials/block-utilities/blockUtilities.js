8d9d999b7cdd1f53c28b827a19ef8d291383dd96
Danial Bleile <danbleile@gmail.com>
2022-02-11 14:33:43 -0800

    Enhancements | Block Utilities | Allow attribute key

--- assets/src/js/partials/block-utilities/blockUtilities.js
+++ assets/src/js/partials/block-utilities/blockUtilities.js
@@ -3,6 +3,7 @@ export { getBlockClassName as getBlockClassName } from './blockClassName';
 export { getBlockClassNameValue as getBlockClassNameValue } from './blockClassName';
 export { hasBlockClassName as hasBlockClassName } from './blockClassName';
 export { removeBlockClassName as removeBlockClassName } from './blockClassName';
+export { setClassName as setClassName } from './blockClassName';
 export { setBlockClassName as setBlockClassName } from './blockClassName';
 export { addBlockClassName as addBlockClassName  } from './blockClassName';
 export { setBlockClassNameBool as setBlockClassNameBool } from './blockClassName';
