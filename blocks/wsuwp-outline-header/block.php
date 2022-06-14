0070d9e57df63c5bee608287302a4a6abe39435e
Danial Bleile <danbleile@gmail.com>
2022-06-13 23:56:09 -0700

    Enhancement | Outline Header | Add

---
+++ blocks/wsuwp-outline-header/block.php
@@ -0,0 +1,79 @@
+<?php namespace WSUWP\Plugin\Gutenberg;
+
+class Block_WSUWP_Outline_Header extends Block {
+
+	protected static $block_name    = 'wsuwp/outline-header';
+	protected static $default_attrs = array(
+		'className'   => 'wsu-decorator',
+		'before'      => '',
+		'outline'   => '',
+		'after'     => '',
+	);
+
+	protected static $chars = array(
+		'a' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">A</text></svg>',
+		'b' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">B</text></svg>',
+		'c' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">C</text></svg>',
+		'd' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">D</text></svg>',
+		'e' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">E</text></svg>',
+		'f' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">F</text></svg>',
+		'g' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">G</text></svg>',
+		'h' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">H</text></svg>',
+		'i' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">I</text></svg>',
+		'j' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">J</text></svg>',
+		'k' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">K</text></svg>',
+		'l' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">L</text></svg>',
+		'm' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">M</text></svg>',
+		'n' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">N</text></svg>',
+		'o' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">O</text></svg>',
+		'p' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">P</text></svg>',
+		'q' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">Q</text></svg>',
+		'r' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">R</text></svg>',
+		's' => '<svg width="55" height="82" viewBox="0 0 50 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">S</text></svg>',
+		't' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">T</text></svg>',
+		'u' => '<svg width="60" height="82" viewBox="0 0 60 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">U</text></svg>',
+		'v' => '<svg width="60" height="82" viewBox="0 0 60 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">V</text></svg>',
+		'w' => '<svg width="70" height="82" viewBox="0 0 62 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">W</text></svg>',
+		'x' => '<svg width="60" height="82" viewBox="0 0 60 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">X</text></svg>',
+		'y' => '<svg width="60" height="82" viewBox="0 0 60 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">Y</text></svg>',
+		'z' => '<svg width="60" height="82" viewBox="0 0 60 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">Z</text></svg>',
+		'\'' => '<svg width="60" height="82" viewBox="0 0 60 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">\'</text></svg>',
+		'-' => '<svg width="60" height="82" viewBox="0 0 60 82"><text x="0" y="63" class="wsu-header__ouline-text" alignment-baseline="top">-</text></svg>',
+	);
+
+
+	public static function render( $attrs, $content = '' ) {
+
+		$outlines = [];
+
+		$outline_sets = explode( ' ', $attrs['outline'] );
+		
+		foreach ( $outline_sets as $index => $outline_set ) {
+
+			$outlines[ $index ] = '';
+
+			$outline_chars = str_split( $outline_set );
+
+			foreach ( $outline_chars as $outline_char ) {
+
+				$outline_char = strtolower( $outline_char );
+
+				if ( array_key_exists( $outline_char, self::$chars ) ) {
+
+					$outlines[ $index ] .= self::$chars[ $outline_char ];
+
+				}
+
+			}
+
+		}
+
+		ob_start();
+
+		include __DIR__ . '/template.php';
+
+		return ob_get_clean();
+
+	}
+
+}
---
+++ blocks/wsuwp-outline-header/editor/edit.js
@@ -0,0 +1,125 @@
+const { __ } = wp.i18n;
+
+const {
+	InnerBlocks,
+	InspectorControls,
+	useBlockProps
+} = wp.blockEditor;
+
+const {
+	TextControl,
+	ToggleControl,
+	SelectControl,
+} = wp.components;
+
+import { 
+	ColorClassControl,
+	SpacingClassNameSelector,
+} from "../../../assets/src/js/partials/block-controls/blockControls";
+
+import { 
+    PanelDisplayOptions,
+	PanelColorOptions,
+} from "../../../assets/src/js/partials/block-panels/blockPanels";
+
+
+import {
+	hasBlockClassName,
+	addBlockClassName,
+	setBlockClassName,
+	getBlockClassNameValue,
+	setChildBlockClassName,
+	setBlockClassNameBool,
+} from "../../../assets/src/js/partials/block-utilities/blockUtilities";
+
+
+const Edit = ( props ) => {
+
+	let {
+		attributes, 
+		setAttributes 
+	} = props;
+
+	const blockProps = useBlockProps( {
+        className: 'wsu-decorator',
+        style: {},
+    } );
+
+	const backgroundColors = [
+        { name: 'gray-5', color: '#f2f2f2' },
+        { name: 'white', color: '#ffffff' },
+        { name: 'gray-85', color: '#262626' },
+    ];
+
+	const borderColors = [
+		{ name: 'default', color: '#e6e6e6' },
+		{ name: 'white', color: '#ffffff' },
+		{ name: 'crimson', color: '#A60F2D' },
+		{ name: 'crimson-light', color: '#CA1237' },
+		{ name: 'autumn', color: '#FF6727' },
+		{ name: 'goldfinch', color: '#F3E700' },
+		{ name: 'vineyard', color: '#AADC24' },
+		{ name: 'pacificsky', color: '#5BC3F5' },
+		{ name: 'midnight', color: '#002D61' },
+    ];
+
+	const decoratorStyles = [
+		{ label: 'Triangle: Crimson Right', value: 'triangle-crimson-right' },
+		{ label: 'Triangle: Crimson Left', value: 'triangle-crimson-left' },
+		{ label: 'Triangle: Gray Right', value: 'triangle-gray-right' },
+		{ label: 'Triangle: Gray Left', value: 'triangle-gray-left' },
+		{ label: 'Block: Gray', value: 'block-gray' },
+		{ label: 'Block: Gray Dark', value: 'block-gray-dark' },
+		{ label: 'Block: Crimson', value: 'block-crimson' },
+		{ label: 'Block: Crimson Light', value: 'block-crimson-light' },
+		{ label: 'None', value: '' },
+	];
+
+
+	const getDecoratorName = () => {
+
+		let value = getBlockClassNameValue( attributes,'wsu-decorator--style-', '' );
+		let name = 'None'
+
+		decoratorStyles.forEach( ( style ) => {
+
+			if ( style.value === value ) {
+				name = style.label;
+			}
+
+		} );
+
+		return name;
+
+	}
+
+
+    return (
+		<>
+		<InspectorControls>
+            <PanelDisplayOptions isOpen={true} >
+			
+				<TextControl
+					label="Fefore Outline"
+					value={ attributes.before }
+					onChange= { ( before ) => setAttributes( { before } ) }
+				/>
+				<TextControl
+					label="Outline"
+					value={ attributes.outline }
+					onChange= { ( outline ) => setAttributes( { outline } ) }
+				/>
+				<TextControl
+					label="After"
+					value={ attributes.after }
+					onChange= { ( after ) => setAttributes( { after } ) }
+				/>
+            </PanelDisplayOptions>
+		</InspectorControls>
+		<h2 { ...blockProps }  >{ attributes.before } { attributes.outline } { attributes.after }</h2>
+		</>
+    )
+
+}
+
+export default Edit;
