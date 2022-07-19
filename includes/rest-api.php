2a0bad37ebd5ff5e6de3167064f89ae7837db5a8
Danial Bleile <danbleile@gmail.com>
2022-07-19 05:04:25 -0700

    Enhancement | API Render | Switch to post

--- includes/rest-api.php
+++ includes/rest-api.php
@@ -27,7 +27,7 @@ class Rest_API {
 			'wsu-gutenberg/v1',
 			'render-block/(?P<block_name>[a-zA-Z0-9-]+)',
 			array(
-				'methods'             => 'GET',
+				'methods'             => array('GET','POST'),
 				'callback'            => array( __CLASS__, 'render_endpoint' ),
 				'permission_callback' => '__return_true', // https://wordpress.org/support/topic/missing-the-required-permission_callback-argument/
 			)
