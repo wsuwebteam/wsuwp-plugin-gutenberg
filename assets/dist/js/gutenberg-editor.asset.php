2933c468257da8756ceaea93684605dd9924248d
Danial Bleile <danbleile@gmail.com>
2022-11-10 16:41:04 -0800

    Version | 1.12.3

--- assets/dist/js/gutenberg-editor.asset.php
+++ assets/dist/js/gutenberg-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'a14f735fc9bd960dd869');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'a9ce9df39904fc631141');
