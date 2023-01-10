e9cd61b8b016af74811a445fca9b365f22dd12d6
Dan White <dan.white1@wsu.edu>
2023-01-10 15:14:04 -0800

    Remove dropcaps for multiple WP versions

--- includes/disable-drop-cap.php
+++ includes/disable-drop-cap.php
@@ -2,15 +2,32 @@
 
 class Disable_Drop_cap {
 
+
+	public static function disable_drop_cap_5_8( $editor_settings ) {
+
+		$editor_settings['__experimentalFeatures']['typography']['dropCap'] = false;
+		return $editor_settings;
+
+	}
+
 	public static function disable_drop_cap( $editor_settings ) {
+
 		$editor_settings['__experimentalFeatures']['global']['typography']['dropCap'] = false;
 		return $editor_settings;
 
 	}
 
 	public static function init() {
-		add_action( 'block_editor_settings', array( __CLASS__, 'disable_drop_cap' ) );
+
+		if ( version_compare( get_bloginfo( 'version' ), '5.8', '>=' ) ) {
+			add_action( 'block_editor_settings_all', array( __CLASS__, 'disable_drop_cap_5_8' ) );
+		} else {
+			add_action( 'block_editor_settings', array( __CLASS__, 'disable_drop_cap' ) );
+		}
+
 	}
+
+
 }
 
 Disable_Drop_cap::init();
