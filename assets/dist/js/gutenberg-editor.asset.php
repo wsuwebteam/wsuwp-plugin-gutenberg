c15c247eebbf76588a65a8a5fa8d775e4ec16dc3
Dan White <dan.white1@wsu.edu>
2023-02-02 13:50:14 -0800

    Build Assets

--- assets/dist/js/gutenberg-editor.asset.php
+++ assets/dist/js/gutenberg-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '483e8ec55b5256ee2aa5');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-polyfill'), 'version' => '875f037fbc7f78f3164bc7fb83892644');
