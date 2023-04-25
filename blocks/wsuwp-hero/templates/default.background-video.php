74535bcb26ee448a4410587eb55a023e352b2609
Danial Bleile <danbleile@gmail.com>
2023-04-25 09:34:32 -0700

    Bug Fix | Video description

--- blocks/wsuwp-hero/templates/default.background-video.php
+++ blocks/wsuwp-hero/templates/default.background-video.php
@@ -1,7 +1,7 @@
 <div class="wsu-video-frame wsu-video-frame--position-fill">
 	<iframe class="wsu-video-frame__video-background" src="https://player.vimeo.com/video/<?php echo esc_attr( $attrs['videoId'] ); ?>?background=1" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen title="<?php echo esc_attr( $attrs['videoTitle'] ); ?>"></iframe>
 	<button class="wsu-button-pause-background wsu-video-frame--action-pause-background" aria-label="Pause Animation"></button>
-	<div class="wsu-visibility-hidden">
+	<div class="wsu-visibility-hidden" style="font-size: 0;opacity: 0">
 		<?php echo wp_kses_post( $attrs['videoDescription'] ); ?>
 	</div>
 </div>
