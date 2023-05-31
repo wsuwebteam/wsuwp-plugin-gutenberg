36ffda5f66cee0600a8882d134587478160ece92
Danial Bleile <danbleile@gmail.com>
2023-05-31 08:57:38 -0700

    Enhancement | Add link to video

--- blocks/wsuwp-news/templates/card.php
+++ blocks/wsuwp-news/templates/card.php
@@ -2,6 +2,7 @@
 	<?php if ( ! empty( $attrs['allowVideo'] ) && ! empty( $card['thumbnailVideo'] ) ) : ?>
 	<div class="wsu-card__video-wrapper" <?php if ( ! empty( $card['imageSrc'] ) ) : ?> style="background-image:url(<?php echo esc_attr( $card['imageSrc'] ); ?>)"<?php endif; ?>>
 		<iframe class="wsu-featured-video wsu-featured-video--vimeo" src="https://player.vimeo.com/video/<?php echo esc_attr( $card['thumbnailVideo'] ); ?>?background=1" width="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen="" title="WSU" style="aspect-ratio: 16 / 9; max-width: 100%;"></iframe>
+		<?php if ( ! empty( $card['link'] ) ) : ?><a class="wsu-card__video-link" href="<?php echo esc_url( $card['link'] ); ?>" style="font-size:0;color:transparent" tabindex="-1" aria-hidden="true"><?php echo wp_kses_post( $card['title'] ); ?></a><?php endif; ?>
 	</div>
 	<?php elseif ( ! empty( $card['imageSrc'] ) ) : ?>
 	<div class="wsu-image-frame wsu-image-frame--fixed-ratio">
