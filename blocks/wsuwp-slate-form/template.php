600f2ed51427b49f2eae32876615415b0d96f230
Danial Bleile <danbleile@gmail.com>
2023-08-04 08:40:04 -0700

    fix: slate form template

--- blocks/wsuwp-slate-form/template.php
+++ blocks/wsuwp-slate-form/template.php
@@ -1,4 +1,4 @@
 <div class="wsu-custom-rfi">
     <div id="form_<?php echo esc_attr( $attrs['formId'] ); ?>">Loading...</div>
 </div>
-<script>/*<![CDATA[*/var script = document.createElement('script'); script.async = 1; script.src = 'https://futurecoug.wsu.edu/register/?id=<?php echo esc_attr( $attrs['formId'] ); ?>&output=embed&div=form_<?php echo esc_attr( $attrs['formId'] ); ?>' + ((location.search.length > 1) ? '&' + location.search.substring(1) : ''); var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(script, s);/*]]>*/</script>
+<script> var script = document.createElement('script'); script.async = 1; script.src = 'https://futurecoug.wsu.edu/register/?id=<?php echo esc_attr( $attrs['formId'] ); ?>&output=embed&div=form_<?php echo esc_attr( $attrs['formId'] ); ?>' + ((location.search.length > 1) ? '&' + location.search.substring(1) : ''); var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(script, s);</script>
