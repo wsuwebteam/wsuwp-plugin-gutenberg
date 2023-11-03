427ad3d9196eded43037242aa012a22a179ac839
Danial Bleile <danbleile@gmail.com>
2023-11-03 08:21:16 -0700

    version: 1.19.11

--- wsuwp-plugin-gutenberg.php
+++ wsuwp-plugin-gutenberg.php
@@ -16,7 +16,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define( 'WSUWPPLUGINGUTENBERGVERSION', '1.19.10' );
+define( 'WSUWPPLUGINGUTENBERGVERSION', '1.19.11' );
 
 add_action( 'after_setup_theme', 'wsuwp_plugin_gutenberg_init' );
 
